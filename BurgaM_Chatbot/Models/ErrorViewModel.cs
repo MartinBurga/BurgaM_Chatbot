ed4b79bdfc7668b43150ab97449c97d5c4c340c0
laboratorio <laboratorio@UPOAULA10716>
2025-05-05 08:32:47 -0500

    Agregar archivos de proyecto.

---
+++ BurgaM_Chatbot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BurgaM_Chatbot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
